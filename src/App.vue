fd2698c3b5bfef75ca95648cdbabc99e9e1458d9
Ariz Kazani <ariz.kazani@sudent.tdsb.on.ca>
2024-05-04 16:35:53 -0400

    added google analytics

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,13 @@
 import Header from '../src/components/Header.vue';
 import Body from '../src/components/Body.vue';
 import Footer from '../src/components/Footer.vue';
-
 </script>
 
 <template>
+
   <Body></Body>
   <Header></Header>
   <Footer></Footer>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDEShTKqO-HUf7tvi9IaVPXCTU1vglW2sw",
   authDomain: "ariz-kazani.firebaseapp.com",
@@ -11,5 +15,8 @@ const firebaseConfig = {
   measurementId: "G-ND3W30HFEY"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { analytics };
