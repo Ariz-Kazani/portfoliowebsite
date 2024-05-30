71c4a2d1d07403ea62f064b15eaf9d5c7b894e37
Ariz Kazani <kazani.ariz@gmail.com>
2024-05-30 11:56:03 -0400

    styling update

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -3,14 +3,15 @@
 </script>
 
 <template>
-<div id="work-con">
-    work con stuff
-</div>
+    <div id="work-con">
+        work con stuff
+    </div>
 </template>
 
 <style scoped>
 #work-con {
     /* background: pink; */
     min-height: 200vh;
+    max-width: 90%;
 }
 </style>
