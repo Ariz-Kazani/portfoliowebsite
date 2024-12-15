d53491bf3447b68d71eb7a5b3f08770714e327a5
Ariz Kazani <kazani.ariz@gmail.com>
2024-12-15 15:00:35 -0500

    started the prototype for the career timeline

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -15,16 +15,6 @@ import php from '../assets/technologies/php.svg';
 
 const work = ref([
   {
-    company: 'ConfiDens Analytics',
-    position: 'Software Developer Intern',
-    type: '',
-    date: 'May 2024 - August 2024',
-    technologies: [laravel, php, vuejs, javascript, html, sass, css, mysql, git],
-    description: `In the summer of 2024 I interned at ConfiDens Analytics as a Software Developer, I've had the opportunity to work on diverse and impactful projects that have significantly enhanced my technical skills and practical knowledge. One of my primary responsibilities was to create the competitor analysis feature. This has required a keen eye for design and a strong understanding of front-end technologies such as SASS, and Vue.js. Additionally, I lead research and development of the competitor analysis feature which has exposed me to both back-end and front-end development.`,
-    id: 'confidens',
-    isShown: false,
-  },
-  {
     company: 'Carleton University',
     position: 'Teaching Assistant',
     type: '',
@@ -34,6 +24,16 @@ const work = ref([
     id: 'carleton-university',
     isShown: false,
   },
+  {
+    company: 'ConfiDens Analytics',
+    position: 'Software Developer Intern',
+    type: '',
+    date: 'May 2024 - August 2024',
+    technologies: [laravel, php, vuejs, javascript, html, sass, css, mysql, git],
+    description: `In the summer of 2024 I interned at ConfiDens Analytics as a Software Developer, I've had the opportunity to work on diverse and impactful projects that have significantly enhanced my technical skills and practical knowledge. One of my primary responsibilities was to create the competitor analysis feature. This has required a keen eye for design and a strong understanding of front-end technologies such as SASS, and Vue.js. Additionally, I lead research and development of the competitor analysis feature which has exposed me to both back-end and front-end development.`,
+    id: 'confidens',
+    isShown: false,
+  },
 ])
 
 </script>
@@ -41,14 +41,18 @@ const work = ref([
 <template>
   <div id="work-con" class="work-con">
     <template v-for="(position, index) in work" :key="position.id">
-      <div id="work-item">
-        <h1 id="work-item-comp">{{ position.company }}</h1>
-        <h2 id="work-item-pos">{{ position.position }}</h2>
-        <h4 id="work-item-aux-info">{{ position.type }} {{ position.date }}</h4>
-        <template v-for="tech in position.technologies" :key="tech">
-          <img :src="tech" alt="" />
-        </template>
-        <p>{{ position.description }}</p>
+      <div id="test">
+        <div id="test-child" :class="{ 'test-child-right': index % 2 == 0, 'test-child-left': index % 2 != 0}">
+          <div id="work-item">
+            <h1 id="work-item-comp">{{ position.company }}</h1>
+            <h2 id="work-item-pos">{{ position.position }}</h2>
+            <h4 id="work-item-aux-info">{{ position.type }} {{ position.date }}</h4>
+            <template v-for="tech in position.technologies" :key="tech">
+              <img :src="tech" alt="" />
+            </template>
+            <p>{{ position.description }}</p>
+          </div>
+        </div>
       </div>
 
     </template>
@@ -67,12 +71,36 @@ img {
   height: 20px;
 }
 
+#test {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  justify-items: center;
+  /* background-color: aqua; */
+}
+
+#test-child {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.test-child-right {
+  grid-column: 2;
+  border-left: 1px solid pink;
+}
+
+.test-child-left {
+  grid-column: 1;
+  border-right: 1px solid pink;
+}
+
 #work-con {
   min-height: 20vh;
   width: 100%;
   padding: 32px 0 32px 0;
-  display: flex;
-  justify-content: space-evenly;
+  display: grid;
+  /* gap: 32px; */
+  /* justify-content: space-evenly; */
 }
 
 #work-con #work-item {
@@ -81,6 +109,8 @@ img {
   width: 40vw;
   background-color: var(--foreground-2);
   box-shadow: 0px 0px 4px 1px var(--surface);
+  /* background-color: pink; */
+
 }
 
 #work-con #work-item:hover {
@@ -101,6 +131,6 @@ img {
     width: 80vw;
     margin: 0 0 128px 0;
   }
-  
+
 }
 </style>
